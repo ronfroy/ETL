b9307b5774e26f04262d6395dc3dcb47fd4f194a
Rudy Onfroy <ronfroy@campings.com>
2018-08-24 11:39:15 +0200

    add transformer chain

---
+++ src/Transformer/TransformerChain.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ETL\Transformer;
+
+class TransformerChain implements TransformerInterface
+{
+    /** @var TransformerInterface[] */
+    private $transformers;
+
+    /**
+     * TransformerChain constructor.
+     *
+     * @param TransformerInterface[] $transformers
+     */
+    public function __construct(array $transformers)
+    {
+        foreach ($transformers as $transformer) {
+            $this->addTransformer($transformer);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function transform($data)
+    {
+        foreach ($this->transformers as $transformer) {
+            $data = $transformer->transform($data);
+        }
+        
+        return $data;
+    }
+
+
+    /**
+     * @param TransformerInterface $transformer
+     */
+    private function addTransformer(TransformerInterface $transformer)
+    {
+        $this->transformers[] = $transformer;
+    }
+}
